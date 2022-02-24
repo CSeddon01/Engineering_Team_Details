b12e1fbc672f00dd03584d7aa1e7dea4a02a90fd
Christy Seddon <89155206+CSeddon01@users.noreply.github.com>
2022-02-24 14:25:08 -0600

    Created employee test and employee information

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,2 +1,10 @@
-const Potion = require('../lib/Employee.js');
+const Employee = require('../lib/Employee.js');
+
+test ("creates employee information object", () => {
+    const employee = new Employee("Jared", 1, "jared@fakemail.com");
+
+    expect(employee.name).toBe("Jared");
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
 
